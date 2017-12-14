fe118b3028d59c82fa5b5120be43547ab727ee42
schitcoski <rschitcoski@gmail.com>
2017-12-14 16:45:10 -0200

    Adicionado Projeto de OO

---
+++ OrientacaoAObjetos/src/TestaCliente.java
@@ -0,0 +1,22 @@
+
+public class TestaCliente {
+	public static void main(String[] args) {
+		Cliente c1 = new Cliente();
+		c1.nome = "Sauron";
+		c1.codigo = 1;
+
+		Cliente c2 = new Cliente();
+		c2.nome = "Gandalf";
+		c2.codigo = 2;
+
+		System.out.println("Dados do primeiro cliente");
+		System.out.println("Nome: " + c1.nome);
+		System.out.println("Código: " + c1.codigo);
+
+		System.out.println("-------------------------------------------------");
+
+		System.out.println("Dados do segundo cliente");
+		System.out.println("Nome: " + c2.nome);
+		System.out.println("Código: " + c2.codigo);
+	}
+}
