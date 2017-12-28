ae86314b858502ca629995a3a7004c53e1e00f6c
schitcoski <rschitcoski@gmail.com>
2017-12-28 15:19:17 -0200

    Adicionado Exercicio de OO

---
+++ OrientacaoAObjetos/src/CartaoDeCredito.java
@@ -0,0 +1,5 @@
+
+public class CartaoDeCredito {
+	public int numero;
+	public String dataDeValidade;
+}
---
+++ OrientacaoAObjetos/src/TestaCartaoDeCredito.java
@@ -0,0 +1,22 @@
+
+public class TestaCartaoDeCredito {
+	public static void main(String[] args) {
+		CartaoDeCredito cdc1 = new CartaoDeCredito();
+		cdc1.numero = 1111111;
+		cdc1.dataDeValidade = "01/01/2023";
+
+		CartaoDeCredito cdc2 = new CartaoDeCredito();
+		cdc2.numero = 222222;
+		cdc2.dataDeValidade = "01/01/2024";
+
+		System.out.println("Dados do primeiro cartão");
+		System.out.println("Número: " + cdc1.numero);
+		System.out.println("Data de validade: " + cdc1.dataDeValidade);
+
+		System.out.println("-----------------------------------------");
+
+		System.out.println("Dados do segundo cartão");
+		System.out.println("Número: " + cdc2.numero);
+		System.out.println("Data de validade: " + cdc2.dataDeValidade);
+	}
+}
